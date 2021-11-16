6667efd4a844879e1fc1e56d4ae91e1ef00b47ad
Darya Kazhekina <daria.kazhekina@gmail.com>
2021-11-17 02:52:24 +0300

    task 19

--- task.js
+++ task.js
@@ -237,3 +237,13 @@
 //   return sum 
 // }
 
+// 19
+
+// function isDivideBy(number, a, b) { 
+//   if(((number % a)===0) && ((number % b)===0)){
+//     return true
+//   } else {
+//     return false
+//   }
+//  } 
+
