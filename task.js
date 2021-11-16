ae623984792683d7530b41d33a4b20d245309ad7
Darya Kazhekina <daria.kazhekina@gmail.com>
2021-11-17 02:40:31 +0300

    task 2

--- task.js
+++ task.js
@@ -5,7 +5,16 @@
 //  }
 
 
-
-
-
+//  2
  
+//  function lovefunc(flower1, flower2){ 
+//   let f1 = flower1 % 2
+//   let f2 = flower2 % 2
+//   if (f1 === f2) {
+//     return false
+//   } else {
+//     return true
+//   }
+// }
+
+
