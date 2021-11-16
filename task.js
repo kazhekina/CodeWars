ce3a9545d620bb34b1bd57fb311fb302ba22d722
Darya Kazhekina <daria.kazhekina@gmail.com>
2021-11-17 02:49:36 +0300

    task 14

--- task.js
+++ task.js
@@ -141,3 +141,18 @@
 //   var name = a + b;
 
 
+//   14
+  
+//   function peopleWithAgeDrink(old) {
+//     if (old < 14) {
+//     return 'drink toddy'
+//     } else if (old < 18) {
+//       return 'drink coke'
+//           } else if (old < 21) {
+//             return 'drink beer'
+//                 } else {
+//                   return 'drink whisky'
+//                 }
+//   }
+
+
