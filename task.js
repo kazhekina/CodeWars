27a3f90d03c823fb7a5a550900c27fad0df97f05
Darya Kazhekina <daria.kazhekina@gmail.com>
2021-11-17 02:47:46 +0300

    task 11

--- task.js
+++ task.js
@@ -116,3 +116,14 @@
 //   }
 
 
+//   11
+  
+//   var summation = function (num) {
+//      var sum = 0
+//      for (i = 1; i <= num; i++) {
+//        sum += i
+//      }
+//      return sum 
+//   }
+
+
