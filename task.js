2353c53b7307e70c077a08fbca9dad9f2cd8006e
Darya Kazhekina <daria.kazhekina@gmail.com>
2021-11-17 04:15:09 +0300

    task 26

--- task.js
+++ task.js
@@ -367,3 +367,22 @@
 //     return result.trim()
 //   }
 
+//   26
+
+//   function isPrime(num) {
+//     if (num < 2) {
+//        return false;
+//     }
+//   if (num === 2) {
+//        return true;
+//     }
+    
+//     const maximumDividor = Math.sqrt(num) + 1;
+//   for (let i = 2; i < maximumDividor; i++) {
+//        if (num % i === 0) {
+//          return false;
+//        }
+//     }
+//   return true;
+//   }
+
