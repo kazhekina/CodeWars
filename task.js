8f82a969ba2910930b39ec94aebe1effb16c8400
Darya Kazhekina <daria.kazhekina@gmail.com>
2021-11-17 04:15:37 +0300

    task 27

--- task.js
+++ task.js
@@ -386,3 +386,19 @@
 //   return true;
 //   }
 
+//   27
+
+//   function count (string) {  
+//     var strObj = {};
+//     var strArr = string.split('');
+//     strArr.map(function(element){
+//       if (strObj.hasOwnProperty(element)) {
+//         strObj[element] = strObj[element] + 1;
+//       } else {
+//         strObj[element] = 1;
+//       }
+//     });
+//     // The function code should be here
+//      return strObj;
+//   }
+
